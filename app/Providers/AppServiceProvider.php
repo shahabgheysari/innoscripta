83631a7d07cc5fca3b01b50dd9822406a5730961
Shahab Gheysari <shahabgheysari@gmail.com>
2023-11-22 00:31:54 +0330

    minor fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,6 @@ namespace App\Providers;
 
 use App\Services\NewsFetchService\Concretes\NewsSourceFetchUrlAdapterGuzzle;
 use App\Services\NewsFetchService\Contracts\NewsSourceFetchUrlAdapterInterface;
-use App\Services\NewsFetchService\NewsAPIOrg\newsAPIOrgFetch;
-use App\Services\NewsFetchService\NewsAPIOrg\NewsAPIOrgFetchUrlAdapter;
-use App\Services\NewsFetchService\NewYorkTimes\NewYorkTimesFetch;
-use App\Services\NewsFetchService\NewYorkTimes\NewYorkTimesFetchUrlAdapter;
-use App\Services\NewsFetchService\TheGuardian\TheGuardianFetch;
-use App\Services\NewsFetchService\TheGuardian\TheGuardianFetchUrlAdapter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,24 +17,6 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
-
-        $this->app->when(newsAPIOrgFetch::class)
-            ->needs(NewsSourceFetchUrlAdapterInterface::class)
-            ->give(function (){
-                return new NewsAPIOrgFetchUrlAdapter();
-            });
-
-        $this->app->when(NewYorkTimesFetch::class)
-            ->needs(NewsSourceFetchUrlAdapterInterface::class)
-            ->give(function (){
-                return new NewYorkTimesFetchUrlAdapter();
-            });
-
-        $this->app->when(TheGuardianFetch::class)
-            ->needs(NewsSourceFetchUrlAdapterInterface::class)
-            ->give(function (){
-                return new TheGuardianFetchUrlAdapter();
-            });
 
         $this->app->bind(NewsSourceFetchUrlAdapterInterface::class,function (){
             return $this->app->make(NewsSourceFetchUrlAdapterGuzzle::class);
