351d8c957f9405c7b653c4ab8ce3926e0a9ef503
Shahab Gheysari <shahabgheysari@gmail.com>
2023-11-23 00:05:27 +0330

    added fetch api command

---
+++ app/Console/Commands/FetchNews.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\NewsFetchService\NewsAggregatorFetchService;
+use Illuminate\Console\Command;
+
+class FetchNews extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:fetch-news';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(NewsAggregatorFetchService $fetchService): void
+    {
+        $this->info('Fetching Just Got Started ...');
+        $fetchService->fetch();
+        $this->info('Messages Has Been Put In Queue.');
+        $this->info('Now Run Queue Work Command To Fetch.');
+    }
+}
